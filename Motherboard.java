8950c16c09cf7ed431dd3836c88a138ba83a33df
N3v3rMor3 <tristanwalker44@gmail.com>
2016-04-01 09:39:32 -0400

    Added files via upload
    random file

---
+++ Motherboard.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Motherboard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Motherboard extends CompParts
+{
+    /**
+     * Act - do whatever the Motherboard wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        setLocation(1003, 537);
+    }    
+    
+}
